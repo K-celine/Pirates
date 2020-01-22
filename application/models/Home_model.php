b135c858f63a13ca2af81a32d5dc489e4b84e0f8
celineK <celinekoeberle@gmail.com>
2020-01-22 11:41:29 +0100

    Affichage dyn des matchs ok

---
+++ application/models/Home_model.php
@@ -0,0 +1,40 @@
+<?php 
+
+
+class Home_model extends CI_Model
+{
+
+
+	public function get_actus()
+	{
+
+		$query = $this->db->get('actus');
+		
+		return $query->result();
+	}
+
+	public function get_matchs()
+	{
+
+		$query = $this->db->get('matchs');
+		
+		return $query->result();
+	}
+		
+
+	public function post_email()
+	{
+		$data = array(
+
+			'email_subscribe' => $this->input->post('email_newsletter')
+		);
+
+		$insert_query = $this->db->insert('newsletter', $data);
+
+		return $insert_query;
+	}
+
+}
+		
+
+ ?>
